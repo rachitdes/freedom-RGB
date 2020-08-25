30b57fa1d261f115a720ff09f89f80c595c279f1
anuj shah <70232742+anujmsee@users.noreply.github.com>
2020-08-25 12:17:21 -0700

    Update RGB_Led.cpp
    I have modified the code and cleaned in a proper way.

--- RGB_Led.cpp
+++ RGB_Led.cpp
@@ -2,13 +2,7 @@
  * @file    RGB_Led.cpp
  * @brief   Application entry point.
  */
-//#include <stdio.h>
-//#include "board.h"
-//#include "peripherals.h"
-//#include "pin_mux.h"
-//#include "clock_config.h"
 #include "MKL25Z4.h"
-//#include "fsl_debug_console.h"
 #include "delay.hh"
 
 #define RED_LED_LOC (1<<18)
@@ -35,17 +29,14 @@ int main ()
 
 		PTB->PSOR = RED_LED_LOC;
 		delay(1000000);
-		PTB->PCOR = RED_LED_LOC;
-		delay(1000000);
-
-
 		PTB->PSOR = GREEN_LED_LOC;
 		delay(1000000);
-		PTB->PCOR = GREEN_LED_LOC;
-		delay(1000000);
-
-
 		PTD->PSOR = BLUE_LED_LOC;
+		delay(1000000);
+		
+		PTB->PCOR = RED_LED_LOC;
+		delay(1000000);
+		PTB->PCOR = GREEN_LED_LOC;
 		delay(1000000);
 		PTD->PCOR = BLUE_LED_LOC;
 		delay(1000000);
